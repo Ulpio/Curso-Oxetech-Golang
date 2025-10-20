a5c01767094d1059ccc8dccf9fd499b522daa80f
Ulpio <ulpio@oxetech.com>
2025-10-20 19:33:57 -0300

    Aula 1 - Ambiente e Hello World: Introducao ao Go, configuracao de ambiente e primeiro programa Hello World

---
+++ Modulo 1 - Basicos/Aula 1 - Ambiente e Hello World/main.go
@@ -0,0 +1,7 @@
+package main // Define como o pacote principal do programa
+
+import "fmt" // Importa o pacote fmt para imprimir na tela
+
+func main() {
+	fmt.Println("Hello, World!") // Imprime "Hello, World!" na tela
+}
